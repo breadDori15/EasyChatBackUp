34cff97f125dcff47a9a465da20c12af6a4c14f3
breadDori15 <ivyyoon15@gmail.com>
2026-03-31 10:18:24 +0900

    [Modify]: html 표현 위치 변경 기존 터미널창에서 출력되던 html 코드를 프로그램 내에서 확인할 수 있도록 변경

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QVBoxLayout, QWidget, QPushButton, QHBoxLayout, QLabel
+from PyQt6.QtWidgets import QApplication, QMainWindow, QSplitter, QTextEdit, QVBoxLayout, QWidget, QPushButton, QHBoxLayout, QLabel
+from PyQt6.QtCore import Qt
 
 class TextEditor(QMainWindow):
     def __init__(self):
@@ -9,37 +10,44 @@ class TextEditor(QMainWindow):
     #프로그램 UI 설정 함수
     def initUI(self):
         self.setWindowTitle('편집기 프로토타입')
-        self.setGeometry(100, 100, 800, 600)
+        self.setGeometry(100, 100, 1000, 600)
 
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
         layout = QVBoxLayout(central_widget)
 
-        self.label = QLabel("채팅 내역 복붙")
-        layout.addWidget(self.label)
-
-        self.editor = QTextEdit()
-        self.editor.setAcceptRichText(True)
-        layout.addWidget(self.editor)
-
-        button_layout=QHBoxLayout()
+        header = QLabel("[왼쪽] 텍스트 입력 및 붙여넣기 [오른쪽] HTML 소스코드 실시간 확인")
+        header.setFixedHeight(30)
+        layout.addWidget(header)
         
-        self.btn_get_html = QPushButton("HTML 소스 보기")
-        self.btn_get_html.clicked.connect(self.show_html_source)
-        button_layout.addWidget(self.btn_get_html)
 
-        self.btn_clear = QPushButton("지우기")
-        self.btn_clear.clicked.connect(self.editor.clear)
-        button_layout.addWidget(self.btn_clear)
+        #body
+        splitter = QSplitter(Qt.Orientation.Horizontal)
 
-        layout.addLayout(button_layout)
+        #body.left
+        self.left_editor = QTextEdit()
+        self.left_editor.setAcceptRichText(True)
+        self.left_editor.setPlaceholderText("여기에 내용을 입력")
+
+        self.left_editor.textChanged.connect(self.update_html_source)
+        
+        #body.right
+        self.right_editor = QTextEdit()
+        self.right_editor.setReadOnly(True)
+        self.right_editor.setStyleSheet("background-color: #2b2b2b; color: #a9b7c6; font-family: Consolas, monospace;")
+        self.right_editor.setPlaceholderText("HTML 소스는 여기에 표시")
+
+        splitter.addWidget(self.left_editor)
+        splitter.addWidget(self.right_editor)
+        splitter.setStretchFactor(0,1)
+        splitter.setStretchFactor(1,1)
+
+        layout.addWidget(splitter)
 
     #HTML 출력 함수
-    def show_html_source(self):
-        html_content = self.editor.toHtml()
-        print("---현재 문서의 HTML 구조---")
-        print(html_content[:500] + "...")
-        self.label.setText("터미널창에서 HTML 소스를 확인하세요")
+    def update_html_source(self):
+        html_content = self.left_editor.toHtml()
+        self.right_editor.setPlainText(html_content)
 
 if __name__ == '__main__': 
     app = QApplication(sys.argv)
